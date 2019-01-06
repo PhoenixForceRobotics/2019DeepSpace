84544bfea1202b134d4be77d30ecfe35c5b618cd
JT Perry <jt.perry@gmail.com>
2019-01-06 14:22:15 -0800

    clean up Motor

--- src/main/java/frc/robot/utility/Motor.java
+++ src/main/java/frc/robot/utility/Motor.java
@@ -1,8 +1,5 @@
 package frc.robot.utility;
 
-import com.ctre.phoenix.ErrorCode;
-import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
 public class Motor extends TalonSRX {
