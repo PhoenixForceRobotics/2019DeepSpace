e5f178136120cc5f1cc0d1ed5bf535e85318da71
Erin McKelvey <erinleemckelvey@gmail.com>
2019-01-19 15:43:40 -0800

    super broken PID

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -1,19 +1,23 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.command.PIDSubsystem;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import frc.robot.Constants;
 import frc.robot.utility.Motor;
 
-public class Elevator extends Subsystem {
+public class Elevator extends PIDSubsystem {
     public Motor elevatorL;
     public Motor elevatorR;
     
     public Elevator() {
-
+        super("Elevator", 2.0, 0.0, 0.0);// The constructor passes a name for the subsystem and the P, I and D constants that are used when computing the motor output
+        setAbsoluteTolerance(0.05);
+        getPIDController().setContinuous(false);
+        
         elevatorL = new Motor(Constants.MotorMap.Elevator.ELEVATORL, Constants.MotorMap.Elevator.ELEVATORL_REVERSED);
-
         elevatorR = new Motor(Constants.MotorMap.Elevator.ELEVATORR, Constants.MotorMap.Elevator.ELEVATORR_REVERSED);
 
+        
     }
     public void initDefaultCommand()
     {
