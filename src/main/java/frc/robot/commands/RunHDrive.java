a7b717748838af913b880a9ecf058aaeefb4c121
Evan Frishholz <evan.frishholz@gmail.com>
2019-01-22 19:57:48 -0800

    add logging to Drivebase Commands

--- src/main/java/frc/robot/commands/RunHDrive.java
+++ src/main/java/frc/robot/commands/RunHDrive.java
@@ -5,12 +5,18 @@ import frc.robot.Robot;
 import frc.robot.subsystems.Drivebase;
 import frc.robot.subsystems.OI;
 
+import java.util.logging.Logger;
+import frc.robot.utility.Log;
+
 public class RunHDrive extends Command
 {
     private Drivebase drivebase;
     private OI oi;
 
+    private static final Logger logger = Log.configureLog(RunHDrive.class.getName());
+
     public RunHDrive(){
+        logger.fine("Spinup RunHDrive");
         requires(Robot.drivebase);
 
         this.drivebase = Robot.drivebase;
@@ -25,6 +31,7 @@ public class RunHDrive extends Command
     @Override
     public void execute()
     {
+        logger.finest("RunHDrive Execute");
         if(oi.driverController.rightTriggerButton.get() & !oi.driverController.leftTriggerButton.get()){
             drivebase.hDrive('r', oi.driverController.triggers.getRight());
         } else if(oi.driverController.leftTriggerButton.get() & !oi.driverController.rightTriggerButton.get()){
@@ -43,12 +50,14 @@ public class RunHDrive extends Command
     @Override
     public void interrupted()
     {
+        logger.fine("RunHDrive Interrupted");
         end();
     }
 
     @Override
     public void end()
     {
+        logger.fine("RunHDrive End");
         drivebase.hDrive('l', 0);
     }
 }
