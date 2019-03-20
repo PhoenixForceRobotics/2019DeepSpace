2b64f251875aa2f7fd1fb6851881d936bfd4fd67
McKelvey <erinleemckelvey@gmail.com>
2019-03-19 19:13:59 -0700

    ur silly

--- src/main/java/frc/robot/commands/elevator/ElevatorPID.java
+++ src/main/java/frc/robot/commands/elevator/ElevatorPID.java
@@ -4,12 +4,16 @@ import frc.robot.Constants;
 import frc.robot.Robot;
 import frc.robot.subsystems.Elevator;
 import edu.wpi.first.wpilibj.command.PIDCommand;
+import frc.robot.subsystems.OI;
 import java.util.logging.Logger;
 import frc.robot.utility.Log;
 
 public class ElevatorPID extends PIDCommand
 {
     private Elevator elevator;
+    private OI oi;
+    private double fineControl;
+    private double newOutput;
 
     private static final Logger logger = Log.configureLog(ElevatorPID.class.getName());
 
@@ -18,6 +22,7 @@ public class ElevatorPID extends PIDCommand
         super(0,0,0);
         requires(Robot.elevator);
         this.elevator = Robot.elevator;
+        this.oi = Robot.oi;
     }
 
     @Override
@@ -27,10 +32,12 @@ public class ElevatorPID extends PIDCommand
 
     @Override
     protected void usePIDOutput(double output) {
-        elevator.elevator1.set(output);
-        elevator.elevator2.set(-output);
-        elevator.elevator3.set(output);
-        elevator.elevator4.set(-output);
+        fineControl = oi.operatorController.leftStick.getY()/10;
+        newOutput = output + fineControl;
+        elevator.elevator1.set(newOutput);
+        elevator.elevator2.set(-newOutput);
+        elevator.elevator3.set(newOutput);
+        elevator.elevator4.set(-newOutput);
     }
 
     public void PIDUp(double setpoint){
